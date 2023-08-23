346ec925d80b4c562d7893a89469ad8753df4b2c
kyrptonaught <anthonybuttillo10@gmail.com>
2023-08-23 15:35:23 -0400

    Initial Commit

---
+++ src/main/java/net/kyrptonaught/serverutilsvelocity/ServerUtilsHandshakeHandler.java
@@ -0,0 +1,60 @@
+package net.kyrptonaught.serverutilsvelocity;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.velocitypowered.api.event.PostOrder;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.event.connection.PostLoginEvent;
+import com.velocitypowered.api.event.connection.PreLoginEvent;
+import com.velocitypowered.api.event.player.ServerLoginPluginMessageEvent;
+import com.velocitypowered.api.network.ProtocolVersion;
+import com.velocitypowered.api.proxy.LoginPhaseConnection;
+import com.velocitypowered.api.proxy.Player;
+
+import java.util.concurrent.TimeUnit;
+
+public class ServerUtilsHandshakeHandler {
+
+    private final Cache<String, byte[]> handshakesPreLogin = CacheBuilder.newBuilder().expireAfterWrite(30, TimeUnit.SECONDS).build();
+    private final Cache<Player, byte[]> handshakes = CacheBuilder.newBuilder().weakKeys().build();
+
+    @Subscribe(order = PostOrder.LAST)
+    public void onPreLogin(PreLoginEvent event) {
+        if (!event.getResult().isAllowed()) return;
+
+        if (event.getConnection().getProtocolVersion().compareTo(ProtocolVersion.MINECRAFT_1_13) < 0) return;
+
+        LoginPhaseConnection conn = (LoginPhaseConnection) event.getConnection();
+
+        conn.sendLoginPluginMessage(ServerUtilsVelocityPlugin.HANDSHAKE_ID, new byte[1], responseBody -> {
+            if (responseBody == null) return;
+
+            handshakesPreLogin.put(event.getUsername(), responseBody.clone());
+        });
+    }
+
+    @Subscribe
+    public void onPostLogin(PostLoginEvent event) {
+        byte[] hs = handshakesPreLogin.getIfPresent(event.getPlayer().getUsername());
+        if (hs != null) {
+            handshakes.put(event.getPlayer(), hs);
+        }
+    }
+
+    @Subscribe
+    public void onServerLoginPluginMessage(ServerLoginPluginMessageEvent event) {
+        if (event.getIdentifier().equals(ServerUtilsVelocityPlugin.HANDSHAKE_ID)) {
+            byte[] hs = handshakes.getIfPresent(event.getConnection().getPlayer());
+            if (hs != null) {
+                event.setResult(ServerLoginPluginMessageEvent.ResponseResult.reply(hs));
+            }
+        }
+    }
+
+    @Subscribe
+    public void onDisconnect(DisconnectEvent event) {
+        this.handshakesPreLogin.invalidate(event.getPlayer().getUsername());
+        this.handshakes.invalidate(event.getPlayer());
+    }
+}
---
+++ src/main/java/net/kyrptonaught/serverutilsvelocity/ServerUtilsVelocityPlugin.java
@@ -0,0 +1,43 @@
+package net.kyrptonaught.serverutilsvelocity;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Dependency;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.proxy.messages.ChannelIdentifier;
+import com.velocitypowered.api.proxy.messages.MinecraftChannelIdentifier;
+
+
+import java.nio.file.Path;
+import java.util.logging.Logger;
+
+@Plugin(
+    id = "serverutils-velocity",
+    name = "ServerUtils Velocity Plugin",
+    version = "0.0.1",
+    authors = {"kyptonaught"}
+)
+public class ServerUtilsVelocityPlugin {
+    public static final ChannelIdentifier HANDSHAKE_ID = MinecraftChannelIdentifier.create("scoreboardplayerinfo", "has_mods_packet");
+
+    private final ProxyServer proxyServer;
+    private final Logger logger;
+    private final Path dataDirectory;
+
+    @Inject
+    public ServerUtilsVelocityPlugin(ProxyServer server, Logger logger, @DataDirectory Path dataDirectory) {
+        this.proxyServer = server;
+        this.logger = logger;
+        this.dataDirectory = dataDirectory;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        proxyServer.getChannelRegistrar().register(HANDSHAKE_ID);
+        proxyServer.getEventManager().register(this, new ServerUtilsHandshakeHandler());
+
+    }
+}
